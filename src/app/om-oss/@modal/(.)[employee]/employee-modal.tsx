ba998f5249a95756139cf54032675ff9b6cbba6c
R C <rickoliwer@gmail.com>
2026-02-02 13:21:18 +0100

    fixes

--- src/app/om-oss/@modal/(.)[employee]/employee-modal.tsx
+++ src/app/om-oss/@modal/(.)[employee]/employee-modal.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import type { getEmployeeBySlug } from "@/lib/fetchers/employees";
 import { useFragment } from "@/graphql";
 import { EmployeeContentFragment } from "@/lib/queries/fragments";
-import Quote from "@/components/blocks/quotes/quote";
+import Quote from "@/components/blocks/quote/quote";
 import EmailLink from "@/components/shared/email-link";
 import { X } from "lucide-react";
 import parse from "html-react-parser";
